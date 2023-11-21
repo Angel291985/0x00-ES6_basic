8260c06fe15f659e18898cf4d2435ff65b6fc11e
Angel291985 <angelsiluma.03@gmail.com>
2023-11-21 20:00:12 +0200

    node

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Thando Siluma <https://github.com/Angel291985>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
